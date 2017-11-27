7c41e87b29a1a6d8b45714dd58c73b500b0e4ebe
piotreqd <piotrduda919@gmail.com>
2017-11-27 19:38:13 +0100

    Creating Project

---
+++ Aproksymacja/Aproksymacja/Aproksymacja.cpp
@@ -0,0 +1,43 @@
+﻿// Aproksymacja.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void DyskretyzujSinus(double dolPrzedziału, double goraPrzedzialu, int wezly, double wartosci[])
+{
+	double przedzial = abs(dolPrzedziału) + abs(goraPrzedzialu);
+	double krok = przedzial / (double)(wezly - 1);
+	double argumentSinusa = dolPrzedziału;
+
+	cout << "krok = " << krok << endl;
+
+	for (int i = 0; i < wezly; ++i)
+	{
+		wartosci[i] = cos(argumentSinusa) * cos(3 * argumentSinusa);
+		cout << "f( " << argumentSinusa << " ) = " << wartosci[i] << endl;
+		argumentSinusa += krok;
+	}
+}
+
+//double Blad(double tabX[], double tabY[], int a[], int m)
+//{
+
+//}
+
+int main()
+{
+	double tabX[50];
+	double tabY[50];
+	const int wezly = 50;
+	int m = 3;
+	double kresDolny = -2.0;
+	double kresGorny = 2.0;
+
+	DyskretyzujSinus(kresDolny, kresGorny, wezly, tabX);
+
+	return 0;
+}
